7ee285f89a2e0017e70f04195b59c67e6c57c37a
Code-C <ccottrel@uvm.edu>
2019-09-02 18:19:07 -0400

    literally what I said on the last commit, I just forgot to add De101

--- De101.py
+++ De101.py
@@ -1,25 +1,37 @@
-from pygameWindow import PYGAME_WINDOW
+import sys
 
-pygameWindow = PYGAME_WINDOW()
+sys.path.insert(0, '..')
 
-def Perturb_Circle_Position():
-    global x, y
-    x = random.randint(100, 400)
-    y = random.randint(100, 400)
-    fourSidedDieRoll = random.randint(1,4)
+import Leap
 
-    if fourSidedDieRoll == 1:
-        y = y - 1
-    elif fourSidedDieRoll == 2:
-        y = y + 1
-    elif fourSidedDieRoll == 3:
-        x = x - 1
-    elif fourSidedDieRoll == 4:
-        x = x + 1
+
+#from pygameWindow import PYGAME_WINDOW
+#
+#pygameWindow = PYGAME_WINDOW()
+#
+#def Perturb_Circle_Position():
+#    global x, y
+#    x = random.randint(100, 400)
+#    y = random.randint(100, 400)
+#    fourSidedDieRoll = random.randint(1,4)
+#
+#    if fourSidedDieRoll == 1:
+#        y = y - 1
+#    elif fourSidedDieRoll == 2:
+#        y = y + 1
+#    elif fourSidedDieRoll == 3:
+#        x = x - 1
+#    elif fourSidedDieRoll == 4:
+#        x = x + 1
+
+controller = Leap.Controller()
 
 while True:
-    pygameWindow.Prepare()
-    Perturb_Circle_Position()
-    pygameWindow.Draw_Black_Circle(x,y)
-    pygameWindow.Reveal()
+    frame = controller.frame()
+    if not frame.hands.is_empty:
+        print ("Hand Detected")
+#    pygameWindow.Prepare()
+#    Perturb_Circle_Position()
+#    pygameWindow.Draw_Black_Circle(x,y)
+#    pygameWindow.Reveal()
 
