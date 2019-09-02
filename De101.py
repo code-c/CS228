a14e2528d2fd04d6fbd28cefa51a2761132aec24
Code-C <ccottrel@uvm.edu>
2019-09-02 16:55:36 -0400

    We got it all worked out in real time draw, and added De101 as a copy of RealTimeDraw

---
+++ De101.py
@@ -0,0 +1,25 @@
+from pygameWindow import PYGAME_WINDOW
+
+pygameWindow = PYGAME_WINDOW()
+
+def Perturb_Circle_Position():
+    global x, y
+    x = random.randint(100, 400)
+    y = random.randint(100, 400)
+    fourSidedDieRoll = random.randint(1,4)
+
+    if fourSidedDieRoll == 1:
+        y = y - 1
+    elif fourSidedDieRoll == 2:
+        y = y + 1
+    elif fourSidedDieRoll == 3:
+        x = x - 1
+    elif fourSidedDieRoll == 4:
+        x = x + 1
+
+while True:
+    pygameWindow.Prepare()
+    Perturb_Circle_Position()
+    pygameWindow.Draw_Black_Circle(x,y)
+    pygameWindow.Reveal()
+
